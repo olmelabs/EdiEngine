0ba5c5fb8bca87e7f8a1c263459df66594252475
olmelabs <olmelabs@gmail.com>
2017-11-27 17:03:17 +0000

    development

---
+++ EdiEngine/Runtime/EdiHlLoop.cs
@@ -0,0 +1,21 @@
+﻿using EdiEngine.Common.Definitions;
+
+namespace EdiEngine.Runtime
+{
+    public class EdiHlLoop : EdiLoop
+    {
+        public EdiHlLoop(MapBaseEntity definition, 
+            EdiLoop parent, 
+            int HL01,
+            int? HL02
+            ) : base(definition, parent)
+        {
+            HL01_HierarchicalIdNumber = HL01;
+            HL02_HierarchicalParentIdNumber = HL02;
+        }
+
+        int HL01_HierarchicalIdNumber { get; }
+
+        int? HL02_HierarchicalParentIdNumber { get; }
+    }
+}
